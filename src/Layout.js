31aa8048c884894bc9e3ea78ad00995c1d349f1b
suncrownfisher <suncrownfisher@gmail.com>
2023-04-16 01:35:14 +0800

    first commit

---
+++ src/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Layout
